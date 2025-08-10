2d9136ecbd072354dd5951cb33e60c1af148be84
shojib116 <Md. Nazmul Hasan Shajib>
2025-08-10 11:36:07 +0600

    completed intial ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { useState, type HTMLAttributes } from "react";
+
 function App() {
-  return <main className="text-2xl">Hello</main>;
+  const [columns, setColumns] = useState([["item1"], ["item2"]]);
+  return (
+    <main className="h-svh w-svw flex gap-20 items-center justify-center">
+      {columns.map((items, cidx) => (
+        <Column key={cidx}>
+          {items.map((item) => (
+            <Item key={item}>{item}</Item>
+          ))}
+        </Column>
+      ))}
+    </main>
+  );
 }
 
 export default App;
+
+function Column({ children, ...props }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className="w-100 border border-gray-400 rounded-2xl p-4 min-h-60"
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+function Item({ children, ...props }: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className="w-full h-20 flex items-center hover:cursor-grab justify-center border rounded-lg border-sky-400 bg-sky-200 font-bold select-none"
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
